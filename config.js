2ecca6ce073571eeb11c5c940aea6f35b33ee4d4
Peter Ingram <peter.ingram@hutchhouse.com>
2015-05-03 10:51:44 +0100

    move config

---
+++ config.js
@@ -0,0 +1,11 @@
+System.config({
+  traceurOptions: {
+    annotations: true,
+    memberVariables: true,
+    types: true
+  },
+  paths: {
+    '*': '*.js',
+    'angular2/*': 'angular2/*'
+  }
+});
