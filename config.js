01e14dc9f8d6e3b9feec6c9c5bd63d44a800ae23
Peter Ingram <peter.ingram@hutchhouse.com>
2015-05-10 16:49:44 +0100

    update jspm

--- config.js
+++ config.js
@@ -3,14 +3,22 @@ System.config({
   "transpiler": "traceur",
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
 System.config({
   "map": {
+    "lodash": "npm:lodash@3.8.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:lodash@3.8.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
   }
 });
 
