6264089e929262df574f5a96de5ce21735442a38
Peter Ingram <peter.ingram@hutchhouse.com>
2015-05-10 18:12:28 +0100

    added Lodash

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,33 @@ import {Component, View, bootstrap} from 'angular2/angular2';
 
 import {NameList} from './NameList';
 
+import{_} from 'lodash';
+
 @Component({
     selector: 'my-app'
 })
+
 @View({
     template: '<h1>Hello {{ name }}</h1> <input (keyup)="changeName(namebox.value)" #namebox value="{{name}}">'
 })
+
 // Component controller
 class MyAppComponent {
-  name: string;
-  
-  constructor() {
-    this.name = 'Pete';
-    console.log(NameList.get());
-  }
 
-  changeName(newName) {
-      this.name = newName;
-  }
+    name:string;
+
+    constructor() {
+        this.name = 'Pete';
+
+        console.log(NameList.get());
+
+        console.log(_.difference([1, 2, 3], [4, 2]));
+
+    }
+
+    changeName(newName) {
+        this.name = newName;
+    }
 
 }
 
